9e4f023ff884966a45da96ecfdb4455609f73ca7
Rudolf Daniel <rudolf.daniel@outlook.com>
2017-11-27 17:47:42 +0100

    Final

--- [week-02]/[day-01]/Exercise-33.py
+++ [week-02]/[day-01]/Exercise-33.py
@@ -2,9 +2,11 @@ nr1 = 23
 
 nr2 = int(input("What number is your guess? "))
 
-if nr2 == nr1:
-    print("You found the number: " + str(nr1))
-elif nr2 > nr1:
-    print("The stored number is lower.")
-elif nr2 < nr1:
-    print("The stored number is higher.")
+while nr2 != nr1:
+    if nr2 > nr1:
+        print("The stored number is lower.")
+        nr2 = int(input("Guess again: "))
+    elif nr2 < nr1:
+        print("The stored number is higher.")
+        nr2 = int(input("Guess again: "))
+print("You find the number: " + str(nr1))
---
+++ [week-02]/[day-01]/Exercise-35.py
@@ -0,0 +1,5 @@
+nr = int(input("Type in a number: "))
+
+for i in range(0, nr):
+    print("% "*4)
+    print(" %"*4)
