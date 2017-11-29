764014d67bd04082f30dd0c43da5317115e0eda0
Rudolf Daniel <rudolf.daniel@outlook.com>
2017-11-29 13:03:46 +0100

    ebed

--- [week-02]/[day-03]/Exercise-3S.py
+++ [week-02]/[day-03]/Exercise-3S.py
@@ -1,21 +1,7 @@
-students = [
-        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
-        {'name': 'Gerzson', 'age': 10, 'candies': 1},
-        {'name': 'Aurel', 'age': 7, 'candies': 3},
-        {'name': 'Zsombor', 'age': 12, 'candies': 5}
-]
+url = "https//www.reddit.com/r/nevertellmethebots"
 
-def many_candies():
-    for i in students:
-        if i["candies"] > 4:
-            print(i["name"] + " has more, than four candies.")
+url = url[0:4] + str(":") + url[6:]
+url = url[:-4]
+url = url + str("odds")
 
-def ave():
-    y = 0
-    for i in students:
-        y += i["candies"]
-    y = y/(len(students))
-    print(y)
-
-many_candies()
-ave()
+print(url)
