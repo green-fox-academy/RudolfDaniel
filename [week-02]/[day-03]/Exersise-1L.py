d252b046d2a77143b6b0573d819906b1356de356
Rudolf Daniel <rudolf.daniel@outlook.com>
2017-12-01 15:38:46 +0100

    nos

--- [week-02]/[day-03]/Exersise-1L.py
+++
@@ -1,5 +0,0 @@
-planet_list = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"]
-
-planet_list = planet_list[0:5] + ["Saturnus"] +  planet_list[6:]
-
-print(planet_list)
--- [week-02]/[day-03]/Exersise-1S.py
+++
@@ -1,5 +0,0 @@
-example = "In a dishwasher far far away"
-
-example = example[0:5] + str("galaxy") + example[15:]
-
-print(example)
