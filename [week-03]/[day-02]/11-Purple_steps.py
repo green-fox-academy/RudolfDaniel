faeae4030bd13bf07a6fa2d70276ddc3ae8dcd6a
Rudolf Daniel <rudolf.daniel@outlook.com>
2017-12-06 11:50:35 +0100

    Eleventh

---
+++ [week-03]/[day-02]/11-Purple_steps.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+def create_squares(a):
+    for i in range(19):
+        square = canvas.create_rectangle(int((1+i)*a), int((1+i)*a), int((2+i)*a), int((2+i)*a), fill='purple')
+
+create_squares(10)
+
+root.mainloop()
