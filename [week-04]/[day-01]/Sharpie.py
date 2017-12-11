b20091593f7e315db953737be79cb8b82c2f157e
Rudolf Daniel <rudolf.daniel@outlook.com>
2017-12-11 17:54:48 +0100

    Sharpie

---
+++ [week-04]/[day-01]/Sharpie.py
@@ -0,0 +1,14 @@
+class Sharpie(object):
+    ink_amount = 100.0
+    def __init__(self, color="", width=0.0):
+        self.color = color
+        self.width = width
+    
+    def use():
+        ink_amount +=1
+
+Santa_Maria = Sharpie(object)
+Santa_Maria.color = "brown"
+Santa_Maria.width = 150.0
+
+print(Santa_Maria.color)
