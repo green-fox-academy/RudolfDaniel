c2768835a9a27de7a4b17102328604e8a911e0d2
Rudolf Daniel <rudolf.daniel@outlook.com>
2017-12-13 12:22:23 +0100

    Sum

--- [week-04]/[day-03]/rudolf_daniel_test.py
+++ [week-04]/[day-03]/rudolf_daniel_test.py
@@ -7,6 +7,18 @@ class TestStringMethods(unittest.TestCase):
         apple = Apple()
         self.assertEqual(apple.get_apple(), "apple")
 
+    def test_summa(self):
+        list_of_numbers = Sum()
+        self.assertEqual(list_of_numbers.summa([]), 0)
+    def test_summa(self):
+        list_of_numbers = Sum()
+        self.assertEqual(list_of_numbers.summa([5]), 5)
+    def test_summa(self):
+        list_of_numbers = Sum()
+        self.assertEqual(list_of_numbers.summa([11, 22, 33]), 66)
+    def test_summa(self):
+        list_of_numbers = Sum()
+        self.assertEqual(list_of_numbers.summa([0]), 0)
 
 if __name__ == '__main__':
     unittest.main()
--- [week-04]/[day-03]/rudolf_daniel_work.py
+++ [week-04]/[day-03]/rudolf_daniel_work.py
@@ -1,3 +1,10 @@
 class Apple(object):
     def get_apple(self):
         return "apple"
+
+class Sum(object):
+    def summa(self, numbers):
+        summarum = 0
+        for i in numbers:
+            summarum += i
+        return summarum
