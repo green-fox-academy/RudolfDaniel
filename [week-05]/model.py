11bcdef1490afb990b86ff9eaf8e488df57ca23f
Rudolf Daniel <rudolf.daniel@outlook.com>
2017-12-19 10:09:32 +0100

    second day

--- [week-05]/model.py
+++ [week-05]/model.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from random import *
 
 class Tile(object):
     def __init__(self, coordinate_x, coordinate_y):
@@ -11,3 +12,34 @@ class Wall(object):
         self.coordinate_x = coordinate_x
         self.coordinate_y = coordinate_y
         self.image = PhotoImage(file = "wall.png")
+
+class Character(object):
+    def __init__(self, coordinate_x, coordinate_y, level, hp, dp, sp):
+        self.coordinate_x = coordinate_x
+        self.coordinate_y = coordinate_y
+        self.level = level
+        self.hp = hp
+        self.dp = dp
+        self.sp = sp
+
+class Hero(Character):
+    def __init__(self, coordinate_x, coordinate_y, level, hp, dp, sp):
+        super().__init__(coordinate_x, coordinate_y, level, hp, dp, sp)
+        self.image_up = PhotoImage(file = "hero-up.png")
+        self.image_right = PhotoImage(file = "hero-right.png")
+        self.image_down = PhotoImage(file = "hero-down.png")
+        self.image_left = PhotoImage(file = "hero-left.png")
+        self.image = self.image_down
+        self.hp = 20 + 3 * randint(1, 6)
+        self.dp = dp
+        self.sp = sp
+
+class Skeleton(Character):
+    def __init__(self, coordinate_x, coordinate_y, level, hp, dp, sp):
+        super().__init__(coordinate_x, coordinate_y, level, hp, dp, sp)
+        self.image = PhotoImage(file = "skeleton.png")
+
+class Boss(Character):
+    def __init__(self, coordinate_x, coordinate_y, level, hp, dp, sp):
+        super().__init__(coordinate_x, coordinate_y, level, hp, dp, sp)
+        self.image = PhotoImage(file = "boss.png")
--- [week-05]/view.py
+++ [week-05]/view.py
@@ -7,11 +7,14 @@ canvas = Canvas(root, width="720", height="720")
 canvas.pack()
 
 mappa = Map()
+dezso = Hero(0, 0, 1, 20, 0, 0)
 
 def drawer(thing):
     image = canvas.create_image(thing.coordinate_x + 72, thing.coordinate_y, anchor=NE, image=thing.image)
 
 for i in range(len(mappa.tiles)):
     drawer(mappa.tiles[i])
+
+drawer(dezso)
 
 root.mainloop()
