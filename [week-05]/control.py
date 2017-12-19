948f6306d4c32cdef2f9e52baf18a8a3d8c8460c
Rudolf Daniel <rudolf.daniel@outlook.com>
2017-12-19 17:32:26 +0100

    hero stays on the map

--- [week-05]/control.py
+++ [week-05]/control.py
@@ -7,20 +7,16 @@ dezso = Hero(0, 0, 1, 20, 10, 10)
 
 for i in range(len(mappa.tiles)):
     drawing.drawer(mappa.tiles[i])
-"""
-drawing.drawer(dezso)
-dezso.move("right")
-drawing.drawer(dezso)
-"""
+
 drawing.drawer(dezso)
 def on_key_press(e):
-    if e.keysym == "Up":
+    if e.keysym == "Up" and dezso.coordinate_y >= 72:
         dezso.move("up")
-    elif e.keysym == "Down":
+    elif e.keysym == "Down" and dezso.coordinate_y <= 640:
         dezso.move("down")
-    elif e.keysym == "Right":
+    elif e.keysym == "Right" and dezso.coordinate_x <= 640:
         dezso.move("right")
-    elif e.keysym == "Left":
+    elif e.keysym == "Left" and dezso.coordinate_x >= 72:
         dezso.move("left")
     drawing.drawer(dezso)
 
