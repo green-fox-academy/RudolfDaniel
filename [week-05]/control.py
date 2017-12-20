b820f2ee0172bd6c47ddd745deb3bd21ea695368
Rudolf Daniel <rudolf.daniel@outlook.com>
2017-12-20 11:01:37 +0100

    hero moving finished

--- [week-05]/control.py
+++ [week-05]/control.py
@@ -11,11 +11,17 @@ for i in range(len(mappa.tiles)):
 drawing.drawer(dezso)
 def on_key_press(e):
     if e.keysym == "Up" and dezso.coordinate_y >= 72:
-        dezso.move("up")
-        dezso.image = dezso.image_up
+        if isinstance(mappa.get_tile(dezso.coordinate_x, dezso.coordinate_y - 72), Wall):
+            dezso.image = dezso.image_up
+        else:
+            dezso.move("up")
+            dezso.image = dezso.image_up
     elif e.keysym == "Down" and dezso.coordinate_y <= 640:
-        dezso.move("down")
-        dezso.image = dezso.image_down
+        if isinstance(mappa.get_tile(dezso.coordinate_x, dezso.coordinate_y + 72), Wall):
+            dezso.image = dezso.image_down
+        else:
+            dezso.move("down")
+            dezso.image = dezso.image_down
     elif e.keysym == "Right" and dezso.coordinate_x <= 640:
         if isinstance(mappa.get_tile(dezso.coordinate_x + 72, dezso.coordinate_y), Wall):
             dezso.image = dezso.image_right
@@ -23,8 +29,11 @@ def on_key_press(e):
             dezso.move("right")
             dezso.image = dezso.image_right
     elif e.keysym == "Left" and dezso.coordinate_x >= 72:
-        dezso.move("left")
-        dezso.image = dezso.image_left
+        if isinstance(mappa.get_tile(dezso.coordinate_x - 72, dezso.coordinate_y), Wall):
+            dezso.image = dezso.image_left
+        else:
+            dezso.move("left")
+            dezso.image = dezso.image_left
     drawing.drawer(dezso)
 
 drawing.canvas.bind("<KeyPress>", on_key_press)
