e0b61524872f9d1c3127a5fc57edf9ec62b7eaab
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-01-02 17:43:23 +0100

    second test

--- [week-06]/[day-01]/poker.py
+++ [week-06]/[day-01]/poker.py
@@ -1,2 +1,5 @@
-class Poker(object):
-    pass
+class Poker(object):   
+    def check_hands(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        return 'White wins! - (High card: Ace)'
