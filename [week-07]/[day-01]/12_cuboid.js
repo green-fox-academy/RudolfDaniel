864f6083f7b308398e1392d2d022c6a31b2c5915
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-01-08 14:33:55 +0100

    13

---
+++ [week-07]/[day-01]/12_cuboid.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var a = 10;
+var b = 10;
+var c = 10;
+
+console.log('Surface Area: ' + (6 * a * b));
+console.log('Volume: ' + (a * b * c));
---
+++ [week-07]/[day-01]/13_seconds-in-a-day.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var current_hours = 14;
+var current_minutes = 34;
+var current_seconds = 42;
+
+console.log(((((23-current_hours)*60)*60)+((60-current_minutes)*60)+(60-current_seconds)));
