ee4b6659cfc313887414ee56aa4747393f19fcde
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-01-09 14:42:09 +0100

    until print elementsd

---
+++ [week-07]/[day-02]/Change_elements.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let af = [4, 5, 6, 7];
+
+for (var i = 0; i < af.length; i++) {
+  console.log(af[i])
+}
---
+++ [week-07]/[day-02]/Swap_elements.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let abc = ['Arthur', 'Boe', 'Chloe'];
+
+abc[0] = 'Chloe';
+abc[2] = 'Arthur';
+
+console.log(abc);
