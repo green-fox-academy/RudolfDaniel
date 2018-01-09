7a26f4c7a7234cb207599fba1d7aee419cd7b309
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-01-09 14:59:21 +0100

    until increment element

--- [week-07]/[day-02]/Change_elements.js
+++ [week-07]/[day-02]/Change_elements.js
@@ -1,7 +1,9 @@
 'use strict';
 
-let af = [4, 5, 6, 7];
+let s = [1, 2, 3, 8, 5, 6];
 
-for (var i = 0; i < af.length; i++) {
-  console.log(af[i])
-}
+s = s.map(function(item) {
+  return item == 8 ? 4 : item;
+})
+
+console.log(s[3]);
