1600d08c498f0d86ec3168436cd262cef7740d17
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-01-10 12:14:39 +0100

    query selector

---
+++ [week-07]/[day-03]/Queryselector-1/queryselector-1.js
@@ -0,0 +1,19 @@
+let king = document.getElementById('b325');
+console.log(king);
+
+let conceited = document.querySelector('.asteroid.b326');
+alert(conceited);
+
+let businessLamp = document.querySelectorAll('.asteroid.big');
+console.log(businessLamp);
+
+let conceitedKing = document.querySelectorAll('section > div');
+for (var i = 0; i < conceitedKing.length; i++) {
+  alert(conceitedKing[i]);
+}
+
+let noBusiness = document.querySelectorAll('div');
+console.log(noBusiness);
+
+let allBusiness = document.querySelector('p');
+alert(allBusiness);
