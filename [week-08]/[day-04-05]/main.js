edaa20cd4ad2cabf35de326f76df8d6ab60c95e0
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-01-19 13:06:09 +0100

    js added

---
+++ [week-08]/[day-04-05]/main.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let httpRequest = new XMLHttpRequest();
+httpRequest.open('GET', 'https://time-radish.glitch.me/posts');
+httpRequest.send(null);
+httpRequest.onreadystatechange = function() {
+  if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+    let response = JSON.parse(httpRequest.response);
+    console.log(response);
+    response.posts.forEach(function (element) {
+      createComment(element)
+    });
+  }
+};
+
+function createComment(element) {
+  let comment = document.createElement('div');
+  comment.classList.add('comment');
+  document.querySelector('.commentbox').appendChild(comment);
+
+  let voter = document.createElement('div');
+  voter.classList.add('voter');
+  comment.appendChild(voter);
+
+  let upvote = document.createElement('img');
+  upvote.classList.add('upvote');
+  upvote.src = 'upvote.png';
+  voter.appendChild(upvote);
+  
+  let counter = document.createElement('p');
+  counter.classList.add('counter');
+  counter.textContent = element.score;
+  voter.appendChild(counter);
+
+  let downvote = document.createElement('img');
+  downvote.classList.add('downvote');
+  downvote.src = 'downvote.png';
+  voter.appendChild(downvote);
+
+  let commenttext = document.createElement('div');
+  commenttext.classList.add('commenttext');
+  comment.appendChild(commenttext);
+
+  let title = document.createElement('h3');
+  commenttext.appendChild(title);
+  title.textContent = element.title;
+}
+
+//createComment();
