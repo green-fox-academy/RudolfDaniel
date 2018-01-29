4df8593f1b21485b4766135b2323961516bb8924
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-01-29 17:09:40 +0100

    mockup audio finished

--- [week-10]/audio_mockup.js
+++ [week-10]/audio_mockup.js
@@ -1,6 +1,21 @@
 'use strict';
 
-let audio = document.querySelector("audio");
+let audio = document.querySelector('audio');
 audio.onloadstart = function() {
-  console.log("Starting to load audio");
+  console.log('Starting to load audio');
 }
+
+let media = document.querySelector('audio');
+
+let button = document.querySelector('button');
+
+button.addEventListener('click', function(e) {
+  if (media.paused) {
+    media.play();
+    console.log('clicked to play');
+  } else {
+    media.pause();
+    console.log('clicked to pause');
+  }
+}, false);
+
