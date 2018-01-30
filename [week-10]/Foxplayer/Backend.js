37e444235211c85f6bf5467981c4b4ae2b552723
Rudolf Daniel <rudolf.daniel@outlook.com>
2018-01-30 17:02:37 +0100

    can add new playlist

--- [week-10]/Foxplayer/Backend.js
+++ [week-10]/Foxplayer/Backend.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use('/', express.static('Public'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/Foxplayer.html');
@@ -14,12 +16,16 @@ const playlists = [
   { 'id': 0, 'title': 'All tracks', 'system': 1},
   { 'id': 1, 'title': 'Favorites', 'system': 1},
   { 'id': 2, 'title': 'Test', 'system': 0},
-  { 'id': 3, 'title': 'Test', 'system': 0},
-  { 'id': 4, 'title': 'Test', 'system': 0},
 ]
 
 app.get('/playlists', (req, res) => {
   res.json(playlists)
+});
+
+app.post('/playlists', (req, res) => {
+  let newPlayList = { 'id': playlists.length, 'title': req.body.title, 'system': 0 };
+  playlists.push(newPlayList);
+  res.json(newPlayList);
 });
 
 app.listen(8000);
